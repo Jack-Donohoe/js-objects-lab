8e80b78b46efdbe1d9cec54ae087a0ce296e5a44
Jack Donohoe <72226393+Jack-Donohoe@users.noreply.github.com>
2022-09-15 12:10:55 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,9 @@
 const aCar = {
     owner : "Joe Bloggs",
+    previous_owners: [
+      {name : "Pat Smith", address : "1 Main Street"},
+      {name : "Sheila Dwyer", address : "2 High Street"}
+    ],
     type : {
       make : "Toyota",
       model : "Corolla",
@@ -11,6 +15,17 @@ const aCar = {
       number : "1058"
     }
   };
+
+  aCar.mileage = "10000" ;
+  aCar.colour = {
+    exterior : "red",
+    interior : {
+      texture : "leather",
+      shade : "cream"
+    }
+  };
   
   console.log(aCar.owner + " drives a " + aCar.type.make);
   console.log("Reg. = " + aCar.registration.year + aCar.registration.countyCode + aCar.registration.number)
+  console.log("It is a " + aCar.colour.exterior + " car, " + aCar.mileage + " mileage, with " + aCar.colour.interior.texture + " interior")
+  console.log('First owner : ' + aCar.previous_owners[0].name + " " + aCar.previous_owners[0].address )
